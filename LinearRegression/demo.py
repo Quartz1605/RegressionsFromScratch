3f4bdef307197b276cee7ccb837690e715dd2e9b
Quartz1605 <prathambarekal05@gmail.com>
2025-08-18 01:48:44 +0530

    Added code for visualizing the loss graph

--- LinearRegression/demo.py
+++ LinearRegression/demo.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
 data_1 = pd.read_csv('New_stud_performance.csv')
 data_1 = data_1.drop(columns='Unnamed: 0')
@@ -17,10 +18,21 @@ X_train,X_test,y_train,y_test = train_test_split(X_scaled,y,test_size=0.2,random
 model = LinearRegression(verbose=True)
 check_self = model.fit(X_train,y_train)
 
+
+loss_data = check_self.history
+loss_series = pd.Series(loss_data)
+plt.plot(loss_data)
+plt.title('Training Loss')
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.show()
+
 y_pred = model.predict(X_test)
 
 print(r2_score(y_test,y_pred))
 # achieved a r2_score of 0.9889833406641303
+
+
 
 
 
